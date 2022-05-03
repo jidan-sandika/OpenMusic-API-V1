4ab5fafac57e495491bb787a7fb1f9bbfadf6387
Jidan <zidansandika06@gmail.com>
2022-05-04 03:31:22 +0700

    initial commit

---
+++ src/utils/index.js
@@ -0,0 +1,14 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable camelcase */
+const mapDBToModel = ({
+    id,
+    name,
+    year,
+  }) => ({
+    id,
+    name,
+    year,
+    });
+  
+  module.exports = { mapDBToModel };
+  
